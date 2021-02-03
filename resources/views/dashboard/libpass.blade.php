afff9faa805b1f3d69113ff3fdd04b6f3a2b6ca0
Khairi Ibnutama <mr.ibnutama@gmail.com>
2021-02-03 10:47:39 +0700

    Pdf Content Type 2

--- resources/views/dashboard/libpass.blade.php
+++ resources/views/dashboard/libpass.blade.php
@@ -3,13 +3,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="Content-Type" content="application/pdf; charset=utf-8"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="{{asset('semantic/semantic.css')}}">
 	<title>Document</title>
-	<style>
-		
-	</style>
+	@php header("Content-type:application/pdf"); @endphp
 </head>
 <body>
 	<div class="ui basic segment">
